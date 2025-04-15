85dfe804235d0c693df9c6225e0ad190965431e4
Nick Popovic <nickpop.development@gmail.com>
2025-04-15 00:47:58 -0400

    made lines a bit lighter (lavender)
    made brighter line numbers to better contrast with the catppuccin theme.

--- .config/nvim/lua/plugins/catppuccin.lua
+++ .config/nvim/lua/plugins/catppuccin.lua
@@ -8,8 +8,17 @@ return {
     config = function()
       require("catppuccin").setup({
         transparent_background = true,
+
+        custom_highlights = function(colors)
+          return {
+            LineNr = { fg = colors.lavender },
+            CursorLineNr = { fg = colors.lavender, bold = true },
+          }
+        end,
       })
+
       vim.cmd.colorscheme "catppuccin-mocha"
     end
   }
 }
+
