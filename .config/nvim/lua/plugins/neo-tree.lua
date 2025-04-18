c67734cba201c1575a5a1065b34b7e57935cb38a
Nick Popovic <nickpop.development@gmail.com>
2025-04-18 14:48:53 -0400

    tabbed buffers
    added tabbed buffers (bufferline.lua) to neovim config as well as
    formatting neo-tree.lua code to make it readablea

--- .config/nvim/lua/plugins/neo-tree.lua
+++ .config/nvim/lua/plugins/neo-tree.lua
@@ -1,13 +1,48 @@
 return {
 	"nvim-neo-tree/neo-tree.nvim",
 	branch = "v3.x",
-	dependencies = {
-		"nvim-lua/plenary.nvim",
-		"nvim-tree/nvim-web-devicons",
-		"MunifTanjim/nui.nvim",
+
+  dependencies = {
+	  "nvim-lua/plenary.nvim",
+	  "nvim-tree/nvim-web-devicons",
+	  "MunifTanjim/nui.nvim",
 	},
-	config = function()
-		vim.keymap.set("n", "<C-n>", ":Neotree toggle<CR>", {})
-		vim.keymap.set("n", "<leader>bf", ":Neotree buffers reveal float<CR>", {})
+
+  config = function()
+	  -- Neo-tree keymaps
+	  vim.keymap.set("n", "<C-n>", ":Neotree toggle<CR>", {})
+	  vim.keymap.set("n", "<leader>bf", ":Neotree buffers reveal float<CR>", {})
+
+    -- Neo-tree setup
+	  require("neo-tree").setup({
+      close_if_last_window = true,
+      enable_git_status = true,
+      enable_diagnostics = true,
+      sources = { "filesystem", "buffers", "git_status" },
+      
+      filesystem = {
+        follow_current_file = {
+        enabled = true,
+        },
+        use_libuv_file_watcher = true,
+      },
+     
+      filtered_items = {
+        visible = true,
+        hide_dotfiles = false,
+        hide_gitignored = false,
+      },
+
+      buffers = {
+        follow_current_file = {
+        enabled = true,
+        },
+        show_unloaded = true,
+      },
+      window = {
+        position = "left",
+        width = 30,
+      },
+	  })
 	end,
-}
+  }
