823523ff0083d1afeb29dd1c1fb416a24133fd17
Nick Popovic <nickpop.development@gmail.com>
2025-08-30 13:11:30 -0400

    neovim: theme picker
    added a theme picker (themery) so the user can choose themes with
    <leader>th

--- .config/nvim/lua/plugins/catppuccin.lua
+++
@@ -1,9 +0,0 @@
-return {
-  "catppuccin/nvim",
-  name = "catppuccin",
-  priority = 1000, -- Make sure to load this before all other start plugins
-  config = function()
-    -- Load the colorscheme here
-    vim.cmd.colorscheme("catppuccin")
-  end,
-}
